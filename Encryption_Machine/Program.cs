4844bd1a7fe2d1d83d889799d6dd874d85a5d18a
ZeroBorn <92119810+ZeroBorn@users.noreply.github.com>
2021-10-18 22:11:09 +0300

    Add files via upload

---
+++ Encryption_Machine/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace Encryption_Machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                       
+            Console.Write("Введите текст: ");
+            var inputText = Console.ReadLine();
+            Console.Write("Введите ключ: ");
+            var password = Console.ReadLine();
+            var encryptedText = VigenereCipher.Encrypt(inputText, password);
+            Console.WriteLine("Зашифрованное сообщение: "+ encryptedText);
+            Console.WriteLine("Расшифрованное сообщение: "+ VigenereCipher.Decrypt(encryptedText, password));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Encryption_Machine/VigenereCipher.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption_Machine
+{
+    public class VigenereCipher
+    {
+        static string Library = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //ABCDEFGHIJKLMNOPQRSTUVWXYZ           abcdefghijklmnopqrstuvwxyz          АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ
+
+        public static string Key(string text, int n)
+        {
+            string str = text;
+            while (str.Length < n)
+            {
+                str += str;
+            }
+
+            return str.Substring(0, n);
+        }
+
+        public static string Vigenere(string text,string pas,bool encrypting = true)
+        {
+            var key = Key(pas, text.Length);
+            var exit = "";
+            var counter = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                var libIndex = Library.IndexOf(text[i]);
+                var codeIndex = Library.IndexOf(key[(i - counter) % key.Length]);
+               
+                if (libIndex < 0)
+                {
+                    exit += text[i].ToString();
+                    counter++;
+                }
+                else
+                {
+                    exit += Library[((Library.Length + libIndex + ((encrypting ? 1 : -1) * codeIndex)) % Library.Length)].ToString();
+                }
+            }
+            return exit;
+        }
+
+        public static string Encrypt(string Messag, string password)
+        {          
+        return Vigenere(Messag, password);
+        }
+        
+        public static string Decrypt(string encryptedMes, string password)
+        {
+        return Vigenere(encryptedMes, password, false);
+        }
+
+    }
+
+}
+
